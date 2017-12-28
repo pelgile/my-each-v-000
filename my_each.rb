5db063c300652ab00290a5e52767e24eab0d0110
Michelle Wiraydi <miwiryadi@gmail.com>
2017-12-28 14:47:20 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,6 @@
-def my_each # put argument(s) here
+def my_each(collections) # put argument(s) here
   # code here
 
-  def my_each(collections)
   i = 0
   while i < collections.length
     yield(collections[i])
